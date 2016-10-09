7ef8b99783bd0dfc29246649f4c9c55007cca7d6
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-10-10 01:46:47 +0800

    update nccu docs

---
+++ ethereum_nccu/README.md
@@ -0,0 +1,78 @@
+# ethereum nccu
+
+## Connect to nccu private blockchain
+
+On Mac OS
+
+### Step 0 : Clean up environment 
+
+Remove two folders. 
+
+```
+rm -rf ~/Library/Ethereum
+rm -rf ~/.ethash
+```
+
+### Step 1 : Install Ethereum
+
+Run
+
+```
+brew tap ethereum/ethereum
+brew install ethereum
+```
+
+### Step 2 : Run genesis block
+
+Download [genesis.json](genesis.json) or copy
+
+```
+{
+   "difficulty": "0x0",
+   "extraData": "0x16888",
+   "gasLimit": "0x999999999",
+   "mixhash": "0x000000000000123123123123123168647572616c65787365646c6578",
+   "nonce": "0x0",
+   "timestamp": "0x0"
+}
+```
+
+Run
+
+```
+geth init genesis.json
+```
+
+### Step 3 : Create Ethereum wallet
+
+Create new account
+
+```
+geth account new
+```
+
+### Step 4 : Download Ethereum wallet
+
+[https://github.com/ethereum/mist/releases/](https://github.com/ethereum/mist/releases/)
+
+### Step 5 : Setup static node
+
+Download [static-nodes.json](static-nodes.json) or copy
+
+```
+[
+"enode://17f920427798fe9a609b2e4f61c73198393157ed73e5ea1c19290675801f0ff7f3df020b22edc7c999b4356149a4ca9e32f3e4d3d7534d01beda11dbff0c85ce@140.119.164.155:30303"
+]
+```
+
+Copy to `~/Library/Ethereum/`
+
+```
+mv /path/to/ ~/Library/Ethereum/
+```  
+
+Then run 
+
+```
+geth --networkid 16888 --port 30303 --nodiscover --maxpeers 25 --nat "any" --rpc --rpccorsdomain "*" --rpcapi "eth,net,web3,debug" --mine --minerthreads 2 --autodag
+```
---
+++ ethereum_nccu/homework1/GradesContract.sol
@@ -0,0 +1,164 @@
+ct GradesContract {
+    address public teacher;
+    
+    mapping (address => Grades) studentGrades;
+    
+    address[] studentIds;
+
+    // Add subjectGrade
+    mapping (address => mapping (string => uint)) subjectGrades;
+
+    // Add subjectNames
+    string[] subjectNames;
+
+    struct Grades {
+        uint ChineseGrade;
+        uint EnglishGrade;
+        uint MathGrade;
+        bool Registered;
+    }
+    
+    function GradesContract() {
+        teacher = msg.sender;
+    }
+    
+    function isTeacher() returns (bool) {
+        return (msg.sender == teacher);
+    }
+    
+    function isGradesOf(address input) returns (bool) {
+        return (msg.sender == input);
+    }
+    
+    // add subject
+    function addSubject(string subjectName) {
+        for(uint x = 0; x < subjectNames.length; x++) {
+            if (!equals(subjectNames[x], subjectName)) {
+               throw;
+            }
+        }
+        subjectNames.push(subjectName);
+    }
+
+    // put subject grade
+    function putSubjectGrade(address studentId, string subjectName, uint subjectGrade) {
+        if (!isTeacher()) {
+            throw;
+        }
+
+        bool find = false;
+        var subjectGradeArray = subjectGrades[studentId];
+        for(uint x = 0; x < subjectNames.length; x++) {
+            if (equals(subjectNames[x], subjectName)) {
+                find = true;
+            }
+        }
+
+        if (find == true) {
+            subjectGradeArray[subjectName] = subjectGrade;
+        } else {
+            throw;
+        }
+
+    }
+
+    // getSubject
+    function getSubjectGrade(address studentId, string subjectName) returns (uint) {
+        if (!(isGradesOf(studentId) || isTeacher())) {
+            throw;
+        }
+
+        bool find = false;
+        var subjectGradeArray = subjectGrades[studentId];
+        for(uint x = 0; x < subjectNames.length; x++) {
+            if (equals(subjectNames[x], subjectName)) {
+                find = true;
+            }
+        }
+
+        if (find == true) {
+            return subjectGradeArray[subjectName];
+        } else {
+            throw;
+        }
+    }
+    
+    function putGrade(address studentId, uint chineseGrade, uint englishGrade, uint mathGrade) {
+        // check permission
+        // only the teacher can call this method
+        if (!isTeacher()) {
+            throw;
+        }
+        
+        // if it is first time registered
+        if (studentGrades[studentId].Registered == false) {
+            // then add to Ids array for tracking or iterating
+            studentIds.push(studentId);
+        }
+        
+        // put to mapping, or if it is already registered then overwrite it
+        studentGrades[studentId] = Grades({
+            ChineseGrade: chineseGrade,
+            EnglishGrade: englishGrade,
+            MathGrade:    mathGrade,
+            Registered:   true
+        });
+    }
+    
+    function getGrade(address studentId) returns (uint ChineseGrade, uint EnglishGrade, uint MathGrade) {
+        // check permission
+        // only the student who owns these grades or the teacher can call this method
+        if (!(isGradesOf(studentId) || isTeacher())) {
+            throw;
+        }
+        
+        var theGrades = studentGrades[studentId];
+        
+        ChineseGrade = theGrades.ChineseGrade;
+        EnglishGrade = theGrades.EnglishGrade;
+        MathGrade    = theGrades.MathGrade;
+    }
+    
+    function getGradesSum(address studentId) returns (uint) {
+        // check permission
+        // only the student who owns these grades or the teacher can call this method
+        if (!(isGradesOf(studentId) || isTeacher())) {
+            throw;
+        }
+        
+        var theGrades = studentGrades[studentId];
+        
+        return theGrades.ChineseGrade + theGrades.EnglishGrade + theGrades.MathGrade;
+    }
+    
+    function getGradesAverage(address studentId) returns (uint) {
+        // check permission
+        // only the student who owns these grades or the teacher can call this method
+        if (!(isGradesOf(studentId) || isTeacher())) {
+            throw;
+        }
+        
+        return getGradesSum(studentId) / 3;
+    }
+    
+    function getClassGradesAverage() returns (uint) {
+        // check permission
+        // only the teacher can call this method
+        if (!isTeacher()) {
+            throw;
+        }
+        
+        // iterate all the grades and calculate its average
+        uint sum = 0;
+        var studentNum = studentIds.length;
+        for (var i = 0; i < studentNum; i++) {
+            sum = sum + getGradesSum(studentIds[i]);
+        }
+        
+        return (sum / studentNum) / 3;
+    }
+    
+    function equals(string x, string y) returns (bool) {
+        return sha3(x) == sha3(y) ? true : false;
+    }
+}
